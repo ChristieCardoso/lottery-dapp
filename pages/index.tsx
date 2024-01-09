a6b07767f92839bf70a8cc2d7b6c980dc54b08ca
ChristieCardoso <christiecardoso@hotmail.com>
2024-01-09 17:48:39 -0300

    function disconnect

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
 import { NextPage } from "next";
 import { Header } from "../components/Header";
 import Head from "next/head";
+import { useAddress } from "@thirdweb-dev/react";
 
 const Home: NextPage = () => {
+  const address = useAddress();
+  console.log(address)
   return (
     <div className="bg-[#091B18] min-h-screen flex flex-col">
       <Head>
